37be797c1ed1cac5736cf4ff7128847c4a241658
emstahh <emmi.juhava@gmail.com>
2024-12-16 15:30:21 +0200

    lisää Menu-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.scss'
 import Item from '../Item'
 import Header from '../Header'
+import Menu from '../Menu'
 
 
 function App() {
@@ -13,11 +14,12 @@ function App() {
         <Item />
         <Item />
         <Item />
+        <Menu />
       </div>
     </>
   )
 
-
+  
 }
 
 export default App
