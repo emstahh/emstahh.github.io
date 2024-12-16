70c1eaa9399f1f1b743ab1886d76f69eee9aab85
emstahh <emmi.juhava@gmail.com>
2024-12-16 15:45:55 +0200

    lisää Content-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import styles from './App.module.scss'
 import Item from '../Item'
 import Header from '../Header'
 import Menu from '../Menu'
+import Content from '../Content'
+
 
 
 function App() {
@@ -10,16 +12,18 @@ function App() {
     <>
       <div className={styles.app}>
         <Header />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
+        <Content>
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+        </Content>
         <Menu />
       </div>
     </>
   )
 
-  
+
 }
 
 export default App
